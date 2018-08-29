0695a279cbca21afd76799149126091bb33e0c61
Jani Luukko <jj.luukko@gmail.com>
2018-08-29 22:11:55 +0300

    start using ramda: extract filtering to own fns

--- app/controllers/reservations.js
+++ app/controllers/reservations.js
@@ -1,18 +1,29 @@
 const ilotalo = require('../integrations/ilotalo')
+const R = require('ramda')
+
+const filterUpcoming =
+  R.filter(r => r.starts && r.starts >= new Date())
+const filterPast =
+  R.filter(r => r.starts && r.starts <= new Date())
+const filterByAssociation = association =>
+  R.filter(r => r.association.toLowerCase() === association.toLowerCase())
 
 const getAllReservations = (req, res) =>
   ilotalo.getReservations()
+    .then(req.params.association ? filterByAssociation(req.params.association) : R.identity)
     .then(reservations => res.json(reservations))
 
 const getUpcomingReservations = (req, res) =>
   ilotalo.getReservations()
-    .then(reservations => reservations.filter(r => r.starts && r.starts >= new Date()))
-    .then(filteredReservations => res.json(filteredReservations))
+    .then(filterUpcoming)
+    .then(req.params.association ? filterByAssociation(req.params.association) : R.identity)
+    .then(reservations => res.json(reservations))
 
 const getPastReservations = (req, res) =>
   ilotalo.getReservations()
-    .then(reservations => reservations.filter(r => r.starts && r.starts <= new Date()))
-    .then(filteredReservations => res.json(filteredReservations))
+    .then(filterPast)
+    .then(req.params.association ? filterByAssociation(req.params.association) : R.identity)
+    .then(reservations => res.json(reservations))
 
 module.exports = {
   getAllReservations,
--- app/server.js
+++ app/server.js
@@ -8,7 +8,11 @@ const {
 } = require('./controllers/reservations')
 
 app.get('/reservations/all', getAllReservations)
+app.get('/reservations/all/association/:association', getAllReservations)
+
 app.get('/reservations/upcoming', getUpcomingReservations)
 app.get('/reservations/past', getPastReservations)
+app.get('/reservations/upcoming/association/:association', getUpcomingReservations)
+app.get('/reservations/past/association/:association', getPastReservations)
 
 app.listen(process.env.PORT || 3000, () => console.log('bmur reservation api listening on port 3000!'))
